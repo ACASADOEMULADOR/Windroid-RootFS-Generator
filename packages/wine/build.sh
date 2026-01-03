9aeefd29f160855f695ae1a9e02629219911e6e0
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-03 10:54:35 -0300

    Optimize Esync for Android: use memfd_create and server FD passing

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=86eb3d006aad27ae7fe30cfa3f809ca076ca3e15
+GIT_COMMIT=73b68c50ad7ff3d672344a2fd9563b2d8c8edb22
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
