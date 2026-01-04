e1d2d44799cdc583e39ac22445f1080a7c441cb9
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-04 17:28:50 -0300

    tentativa compilaçao wine

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=eaa95b6d35f64de994ee3781af2c22c42148e571
+GIT_COMMIT=180118c7624303e898a4e71f25f9a2b6031c600f
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
