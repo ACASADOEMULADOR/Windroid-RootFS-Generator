71b783dbb232ed7f0c5d0dcc97ccdbb2309371f4
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-05 22:17:21 -0300

    kernel32: Fix header order in ceg.c to resolve compilation errors

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=3131eb029e7a1e72c101ff0658a5c9aab3e98322
+GIT_COMMIT=8757db77d388d252577d8805331485f48483f421
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
