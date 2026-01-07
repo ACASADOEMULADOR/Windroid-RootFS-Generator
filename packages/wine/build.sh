e9de71c8ab3a6d7e1cac89faa723bc861e4952c7
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-06 21:23:05 -0300

    Update GIT_COMMIT to new commit hashntdll: Implement WINE_DISABLE_SFN environment variable and Yakuza 5 hack

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=6e24671e9536986f6747ffaca48d5996fa76238a
+GIT_COMMIT=9d71ad798b2073c1126eb219c454f8a2b276a021
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
