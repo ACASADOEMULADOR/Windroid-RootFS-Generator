bd254a0a5da09b834170d14a1d1f60aaef05ac88
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-13 22:48:02 -0300

    ntdll: Generalize Android home directory path in setup_config_dir

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=cb76a8b8cec39b94406bdb6534ba9326c37c87b3
+GIT_COMMIT=4aa29c3fb3c26a330eed955d2a5a0f25f4abc5e6
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
