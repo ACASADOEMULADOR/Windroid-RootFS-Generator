856d2a076ce1b759070d6acacfc36e45eef3a9f3
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-16 22:07:48 -0300

    d3d11: Implement restoring of state after executing a command list - Added MAX_WINED3D_STREAMS constant and d3d11_state structure - Implemented state_capture() to save all D3D11 device context state - Implemented state_apply() to restore all D3D11 device context state - Modified ExecuteCommandList to capture/restore state when restore_state is TRUE - Adapted patch from Wine 2.x to Wine 10.x architecture

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=bc726720b9ba83d1e014dc45dd04b45d4e2144aa
+GIT_COMMIT=cf238739aefd8f436763550a67e5847aa930826e
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
