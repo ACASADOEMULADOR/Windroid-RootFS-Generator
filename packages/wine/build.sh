c000734b9527fa02aaeb8e70d606090adadd05f7
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-20 14:29:29 -0300

    wine Fix mountmgr compilation errors

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=a3345f20f4a868f751a1cbb6633ce58281c399cc
+GIT_COMMIT=b8ed956e4c3215f17c9f1650d3f7b98c3c49e1af
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
