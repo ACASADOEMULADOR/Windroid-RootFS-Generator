d44ff28f561e775604e3b98c2c5a713b95cd834e
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-20 01:04:28 -0300

    wine Fix ntdll and dinput compilation errors

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=b709c369b37d666fb99b9f08d26f11dcfc353d77
+GIT_COMMIT=72bb92485d8ee2a3d5c9a8df81d192c8f450cf47
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
