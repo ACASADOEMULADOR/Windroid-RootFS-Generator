7748a8b5842ebd6cfeacd51043bbba26f5d6fe7a
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-22 22:32:02 -0300

    loader: Use memfd_create to avoid noexec filesystem issues

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=0de174e7b0c385b186af9ed7789a0209cf579f59
+GIT_COMMIT=8c4eb00504bcc2bfa29dc0b7fdb529d221c3acb4
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
