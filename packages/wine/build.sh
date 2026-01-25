3586eebe51b4e15e01038e4834743e726fb5577e
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-25 16:39:28 -0300

    Wine ntdll: Implement memfd_create fallback for PROT_EXEC mappings

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=8c4eb00504bcc2bfa29dc0b7fdb529d221c3acb4
+GIT_COMMIT=3a35f210f076e8414ea8caacc65847e38b34af8d
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
