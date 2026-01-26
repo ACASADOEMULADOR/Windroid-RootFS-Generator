e7cce4d3986a45e40daa26c3c729687869e513ff
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-01-25 23:03:18 -0300

    Wine: esync: Add Android ashmem support for improved performance teste

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=9593d977a1c148ba772b05027e8c1b0ab25a9205
+GIT_COMMIT=2ebb3c337ceb351678c8f5457b219aae794e8861
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
