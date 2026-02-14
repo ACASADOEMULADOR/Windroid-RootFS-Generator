784702c0004259f0b1115938fba1760541233624
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-14 13:07:23 -0300

    Wine ntdll, server: Implement eventfd fallback for esync.

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=0a73793e743e05a9a2ca4da1db7b5c7a39f1f0e0
+GIT_COMMIT=3b49963220d296f37ff51162dbe3c83c5ec302bc
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
