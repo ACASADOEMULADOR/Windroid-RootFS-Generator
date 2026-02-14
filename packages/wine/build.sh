5041eb0e943d23414dd70ddd3c768d19825da530
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-14 14:19:00 -0300

    Wine ntdll: Fix undeclared identifier 'PAGE_NOACCESS' in virtual.c

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=3b49963220d296f37ff51162dbe3c83c5ec302bc
+GIT_COMMIT=700051a8fcb73618cef32d28ef831dfc6369aa9e
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
