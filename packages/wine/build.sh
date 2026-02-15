324b365308a6fbc255c9e1fa5d2ec6a045e3eac8
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-15 16:32:10 -0300

    Wine Update memory protection flags in anon_mmap_fixed

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=3b49963220d296f37ff51162dbe3c83c5ec302bc
+GIT_COMMIT=e710f58276fe93d3158acf66cb0e30eebb776011
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
