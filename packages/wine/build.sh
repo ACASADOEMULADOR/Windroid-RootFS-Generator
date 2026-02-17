bd50019403ae160fc303305acab3918df1d20d76
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-17 18:34:03 -0300

    Banjo Recompiled teste

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=ec489ea6cd22083e92e4e2214aeecb925c66ea52
+GIT_COMMIT=84a4219719a71ce921c88e0bfaec343ccd865c33
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
