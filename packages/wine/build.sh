fb0282596694271b1f21eae29e983c675df84c35
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-18 00:42:02 -0300

    Wine esync use memfd_create for shared memory on Android

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=84a4219719a71ce921c88e0bfaec343ccd865c33
+GIT_COMMIT=d3a80b39e93520f067b5216293f2de763c94f802
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
