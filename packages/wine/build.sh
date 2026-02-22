048d48d1d77ed6f9a075d04d6b580abf91c019d7
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-21 22:30:50 -0300

    Implement timeout in joystick device enumeration and creation
    essa mudança e para um teste do jogo prototype que fica em loop

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=064383a758ad4412d354a067126305930fa183aa
+GIT_COMMIT=f297eb21b64d4169e2f687fb9ff46d43a0f47863
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
