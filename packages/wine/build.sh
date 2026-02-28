2d3a7a49176def6a3a31dab89713f95c498aa7f1
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-28 00:34:15 -0500

    TESTE DE FIX WINE ANDROID BIONIC
    Add Android-specific fallback for XShmCreateImage
    Handle System V IPC restrictions on Android gracefully.

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=8e24f78fe67568b381676a7d59d386aef9998d0a
+GIT_COMMIT=7145fc5f3fa65a9c4982501dadf9f0f07c6d3626
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
