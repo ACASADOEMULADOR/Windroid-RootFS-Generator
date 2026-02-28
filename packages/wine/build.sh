dbee913850fd38d4c058681aa7cc90e8656d405f
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-28 18:36:35 -0500

    Update server directory path for Android
    a criação do prefixo entrava em loop

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=24db5163e70dfed0bb60895bd4fe33f3b4884ac4
+GIT_COMMIT=f15f1e68a42f8bac04053e98d27790eb0c866eab
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
