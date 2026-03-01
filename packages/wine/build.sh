eb1dae3c025e6f635703c0040831cfcaf97a018f
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-03-01 12:26:57 -0500

    Disable exception handling for invalid handle
    WOW64 syscall.c modificado teste BO2 01/03/26

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=7145fc5f3fa65a9c4982501dadf9f0f07c6d3626
+GIT_COMMIT=fae65e1ab4559f44a0cbe2064a978ab4379dafdf
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
