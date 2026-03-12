06577b7802c64ead58d2b3d4e13cfd316efa5aaa
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-03-11 23:16:28 -0300

    segunda commit da wine
    numero 064383a758ad4412d354a067126305930fa183aa

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=bada7bff8f6d2d1ddb12ce905cded77b595daca2
+GIT_COMMIT=064383a758ad4412d354a067126305930fa183aa
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
