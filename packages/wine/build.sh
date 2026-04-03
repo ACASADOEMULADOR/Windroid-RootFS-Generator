1b3681116e34297ec4265f68aff1e96033ba1642
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-04-03 11:32:24 -0300

    Reject D3D12 interface queries to prevent crashes
    Added a hack to reject D3D12 interface queries to prevent crashes when using D3D9On12.

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=5c356a0eb3abaccc5b9765f8366239cfdc3e76e8
+GIT_COMMIT=a27ef98a600d96ba1283ac363c44895f0192fcff
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
