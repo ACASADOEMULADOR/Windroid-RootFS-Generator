7066cb60bd6fc39d6fbd63409553eb25325cd139
Windroid-emu <genrogamer@gmail.com>
2026-04-12 16:30:25 -0300

    fix: use Debian mirror for cabextract URL
    The GitHub URL was not returning a valid tarball. Using the
    debian.org mirror which is reliable and provides the correct
    1.11 source archive.

--- packages/cabextract/build.sh
+++ packages/cabextract/build.sh
@@ -1,5 +1,5 @@
 PKG_VER="1.11"
 PKG_CATEGORY="System"
-SRC_URL="https://github.com/kyz/libmspack/releases/download/v1.11/cabextract-1.11.tar.gz"
+SRC_URL="https://ftp.debian.org/debian/pool/main/c/cabextract/cabextract_1.11.orig.tar.gz"
 CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE"
 DEPENDENCIES=""
