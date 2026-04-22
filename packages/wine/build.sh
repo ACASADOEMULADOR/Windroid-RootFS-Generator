257c15681f3a98a7c20d677073570c3553edfd9c
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-04-22 00:36:48 -0300

    Fix FSR RCAS
    implement real-time sharpness adjustment

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=e9b7149e22232fe4a78cf789d1d05c9dde5b7a1a
+GIT_COMMIT=8f5b365a8ceea7e96f277477f5b7a20e30856eb0
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
