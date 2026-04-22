2e0dc7b10c8f451fb865bb51fb419e5672ede914
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-04-21 22:06:20 -0300

    Wine
    winex11.drv: Implement auto-scaling Native FSR Support (FShack-like)

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=f5ddc477375f2037de2177ac3c52ee93d9e94cb1
+GIT_COMMIT=b231e5ab6e35810d57b3e1d7caa64d57b576ea27
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
