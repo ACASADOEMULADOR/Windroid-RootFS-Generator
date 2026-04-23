6e9b3510b3f54816f53af770bc6ed23deb0aeb7e
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-04-23 14:17:20 -0300

    Wine
    ntdll: Implementar HideWineExports e spoofing de ProcessDebugPort para Anti-Cheat

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=8f5b365a8ceea7e96f277477f5b7a20e30856eb0
+GIT_COMMIT=bcb95f51baaaee158da92e705540e60542294f3f
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
