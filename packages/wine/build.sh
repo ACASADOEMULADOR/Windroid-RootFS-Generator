78cae6189cd0a02736ad62bb8b478f4508ef23ce
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-04-25 17:01:10 -0300

    Wine proton wow64 EBP
    ntdll: Recover EBP from WOW64 context when Box64 delivers RBP=0 signal

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroidr-Proton
-GIT_COMMIT=96237e82fa506795f019fcdebf96aff74cd4be12
+GIT_COMMIT=6d0eefbb68c48ef597fc32a67793a63ba0030855
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
