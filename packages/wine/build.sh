d07f13e54cacb123c13c27bd66462fb96f58a7df
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-05-01 13:11:45 -0300

    Wine
    dlls/ntdll: Synchronize address space limits with 39-bit Android kernels
    - Capped address_space_limit to host capability.
    - Adjusted mmap reservations to respect actual addressable memory space.

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=36d3b240edfdbdb9edb1c88283f6ccc57f81b8c9
+GIT_COMMIT=14531b32566f3ad6f2cf509d3c4ed24ebac554e8
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
